248dc78d2d95d85e51b10dec7c3c9ff08430c498
Setharkk <samir.sbjd@gmail.com>
2025-01-13 18:57:46 +0100

    Suppression des fichiers Render

--- frontend/server.js
+++ frontend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import process from 'node:process';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
